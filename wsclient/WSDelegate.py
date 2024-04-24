3315db00e4812296c7acb91823ce3b197424128d
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2024-04-24 21:13:54 +0200

    fix(wsclient): add print of the delegate

--- wsclient/WSDelegate.py
+++ wsclient/WSDelegate.py
@@ -1,12 +1,16 @@
 class WSDelegate:
     def on_open(self):
+        print("Connection opened")
         pass
 
     def on_message(self, message):
+        print(f"Message received: {message}")
         pass
 
     def on_error(self, error):
+        print(f"An error occur: {error}")
         pass
 
     def on_close(self):
+        print("Connection closed")
         pass
