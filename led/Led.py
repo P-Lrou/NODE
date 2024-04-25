d7f33b08eaa5fceb1382a21c57b95aea57b98734
Kibishi47 <moulin.emmanuel47@gmail.com>
2024-04-25 18:49:18 +0200

    refactor(led): led structure

--- led/Led.py
+++ led/Led.py
@@ -1,39 +1,78 @@
 import RPi.GPIO as GPIO
 import time
 
+class LedDictionnary:
+    led_pins = {
+        17: "rouge",
+        27: "rouge",
+        22: "rouge",
+        6 : "vert",
+        13: "vert",
+        19: "vert",
+        26: "vert"
+    }
+
 class LEDController:
     def __init__(self, pin_numbers):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        self.leds = pin_numbers
-        for pin in self.leds:
-            GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin, GPIO.LOW)
 
-    def on(self, led_index):
-        if 0 <= led_index < len(self.leds):
-            GPIO.output(self.leds[led_index], GPIO.HIGH)
+        self.leds = []
+        for pin_number in pin_numbers:
+            self.leds.append(Led(pin_number))
+        self.counter = 0
 
-    def off(self, led_index):
-        if 0 <= led_index < len(self.leds):
-            GPIO.output(self.leds[led_index], GPIO.LOW)
+    def test_all(self):
+        for led in self.leds:
+            led.on()
+            time.sleep(0.7)
+        for led in self.leds:
+            led.off()
+            time.sleep(0.7)
 
-    def toggle(self, led_index):
-        if 0 <= led_index < len(self.leds):
-            current_value = GPIO.input(self.leds[led_index])
-            GPIO.output(self.leds[led_index], not current_value)
+    def on(self):
+        led = self.leds[self.counter]
+        led.on()
+        if self.counter < len(self.leds)-1:
+            self.counter += 1
+
+    def off(self):
+        led = self.leds[self.counter]
+        led.off()
+        if self.counter > 0:
+            self.counter -= 1
+
+    def toggle(self):
+        for led in self.leds:
+            led.toggle
 
     def all_on(self):
         for led in self.leds:
-            GPIO.output(led, GPIO.HIGH)
+            led.on()
 
     def all_off(self):
         for led in self.leds:
-            GPIO.output(led, GPIO.LOW)
+            led.off()
+
+class Led:
+    def __init__(self, pin_number) -> None:
+        self.pin_number = pin_number
+        GPIO.setup(self.pin_number, GPIO.OUT)
+        GPIO.output(self.pin_number, GPIO.LOW)
+        self.state = GPIO.LOW
+    
+    def on(self):
+        GPIO.output(self.pin_number, GPIO.HIGH)
+        self.state = GPIO.HIGH
+    
+    def off(self):
+        GPIO.output(self.pin_number, GPIO.LOW)
+        self.state = GPIO.LOW
+
+    def toggle(self):
+        GPIO.output(self.pin_number, not self.state)
+
 
 if __name__ == "__main__":
-    led_pins = [17, 27, 22, 6, 13, 19, 26]
-    led_controller = LEDController(led_pins)
-    led_controller.all_on()
-    time.sleep(1)
+    led_controller = LEDController(LedDictionnary.led_pins.keys())
     led_controller.all_off()
