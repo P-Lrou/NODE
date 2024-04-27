fa1e2f1e20d71eea6890885f4f80d0dcb91e7bc2
Pierre-Louis ROUSSEAUX <pl74rou@gmail.com>
2024-04-27 15:01:06 +0200

    feat(server): add activities managment

--- Server/Activities/ActivitiesManager.py
+++ Server/Activities/ActivitiesManager.py
@@ -1,11 +1,58 @@
-from tools.DLog import DLog
+import json
 
 
 class ActivitiesManager:
-
     def __init__(self):
-        pass
+        self.activities = {}
+        self.load_activities()
 
-    def new_activity(self, activity):
-        # for the moment we juste print the activity waiting the activites managment system
-        DLog.Log(f"{activity}")
+    def load_activities(self):
+        try:
+            with open("activities.json", "r") as file:
+                self.activities = json.load(file)
+        except FileNotFoundError:
+            self.activities = {
+                "sewing": {"required_participants": 4, "participants": []},
+                "poker": {"required_participants": 6, "participants": []},
+                "chess": {"required_participants": 2, "participants": []}
+            }
+            self.save_activities()
+
+    def reset_activities(self):
+        self.activities = {
+            "sewing": {"required_participants": 4, "participants": []},
+            "poker": {"required_participants": 6, "participants": []},
+            "chess": {"required_participants": 2, "participants": []}
+        }
+        self.save_activities() 
+
+    def save_activities(self):
+        with open("activities.json", "w") as file:
+            json.dump(self.activities, file)
+
+    def handle_activity(self, data, client):
+        activity_type = data.get('activity_type')
+        if activity_type in self.activities:
+            self.activities[activity_type]['participants'].append(
+                {"id": client['id'], "uid": client["uid"]})
+            self.save_activities()
+            return activity_type
+        return None
+
+    def get_participants_count(self, activity_type):
+        if activity_type in self.activities:
+            return len(self.activities[activity_type]['participants'])
+        return 0
+
+    def check_activity_full(self, activity_type):
+        if activity_type in self.activities:
+            return len(self.activities[activity_type]['participants']) >= self.activities[activity_type]['required_participants']
+        return False
+
+    def get_participants(self, activity_type):
+        if activity_type in self.activities:
+            return self.activities[activity_type]['participants']
+        return []
+
+    def activity_exists(self, activity_type):
+        return activity_type in self.activities and len(self.activities[activity_type]['participants']) > 0
--- Server/Message/MessageHandler.py
+++ Server/Message/MessageHandler.py
@@ -1,26 +1,46 @@
 from Activities.ActivitiesManager import ActivitiesManager
 import json
 
-
 class MessageHandler:
-
     def __init__(self):
         self.activitiesManager = ActivitiesManager()
+        self.activitiesManager.reset_activities()
 
     def process_message(self, message, server, client):
         data = json.loads(message)
-        if 'type' in data:
-            if data['type'] == 'activity':
-                if 'activity_type' in data:
-                    if data['activity_type'] == 'chess':
-                        self.activitiesManager.new_activity("chess")
-                    elif data['activity_type'] == 'poker':
-                        self.activitiesManager.new_activity("poker")
-                    elif data['activity_type'] == 'sewing':
-                        self.activitiesManager.new_activity("sewing")
-                    else:
-                        return {"error": "Unknown activity type"}
-                else:
-                    return {"error": "Activity type not specified"}
+        data_content = json.loads(data["text"])
+        if 'type' in data_content and data_content['type'] == 'activity':
+            activity_type = data_content.get('activity_type')
+            if activity_type:
+                is_new_activity = not self.activitiesManager.activity_exists(activity_type)
+                activity_id = self.activitiesManager.handle_activity(data_content, client)
+                if activity_id:
+                    if is_new_activity:
+                        creation_message = json.dumps({"type": "activity_created", "activity_type": activity_type})
+                        for c in server.clients:
+                            if c["id"] != client["id"]:
+                                server.send_message(c, creation_message)
+
+                    participants = self.activitiesManager.get_participants(activity_id)
+                    participants_count = self.activitiesManager.get_participants_count(activity_id)
+                    new_participant_message = json.dumps({"type": "new_participant", "activity_type": activity_id, "count": participants_count})
+                    
+                    # Notify all current participants that a new player has joined
+                    for participant in participants:
+                        if participant["id"] != client["id"]:  # Assuming each 'participant' dict has an 'id'
+                            # Find the client object corresponding to participant['id']
+                            target_client = next((c for c in server.clients if c['id'] == participant["id"]), None)
+                            if target_client:
+                                server.send_message(target_client, new_participant_message)
+                        
+                    # If the activity is full, inform all participants
+                    if self.activitiesManager.check_activity_full(activity_id):
+                        complete_message = json.dumps({"type": "activity_full", "activity_type": activity_id})
+                        for participant in participants:
+                            target_client = next((c for c in server.clients if c['id'] == participant["id"]), None)
+                            if target_client:
+                                server.send_message(target_client, complete_message)
+            else:
+                server.send_message(client, json.dumps({"error": "Activity type not specified"}))
         else:
-            return {"error": "Unknown message type"}
+            server.send_message(client, json.dumps({"error": "Unknown message type"}))
