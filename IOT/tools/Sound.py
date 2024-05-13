867612fe14be2b9ca2a239dbe269064494012314
Kibishi47 <moulin.emmanuel47@gmail.com>
2024-05-13 10:21:04 +0200

    feat(sound): sound volume management

--- IOT/tools/Sound.py
+++ IOT/tools/Sound.py
@@ -7,21 +7,35 @@ class PlaySound:
     error_file = 'import/sound/error.wav'
 
     @staticmethod
-    def play_sound(file):
+    def set_volume(volume):
+        # Assurez-vous que le volume est une valeur entre 0 et 100
+        volume = max(0, min(100, volume))
+        subprocess.run(['amixer', 'sset', 'Master', f'{volume}%'])
+
+    @staticmethod
+    def play_sound(file, volume=100):
+        PlaySound.set_volume(volume)
         subprocess.run(['aplay', file])
 
     @staticmethod
     def join():
-        PlaySound.play_sound(PlaySound.join_file)
+        volume = 50
+        PlaySound.play_sound(PlaySound.join_file, volume)
     
     @staticmethod
     def leave():
-        PlaySound.play_sound(PlaySound.leave_file)
+        volume = 100
+        PlaySound.play_sound(PlaySound.leave_file, volume)
     
     @staticmethod
     def print():
-        PlaySound.play_sound(PlaySound.print_file)
+        volume = 80
+        PlaySound.play_sound(PlaySound.print_file, volume)
     
     @staticmethod
     def error():
-        PlaySound.play_sound(PlaySound.error_file)
+        volume = 70
+        PlaySound.play_sound(PlaySound.error_file, volume)
+
+if __name__ == "__main__":
+    PlaySound.join()
