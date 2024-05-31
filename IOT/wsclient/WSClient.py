4a26f8524b800d4d145f95072997e35b4d33a8cc
Pierre-Louis ROUSSEAUX <pl74rou@gmail.com>
2024-05-31 11:43:22 +0200

    feat(IOT): add server reconnection for the RPI

--- IOT/wsclient/WSClient.py
+++ IOT/wsclient/WSClient.py
@@ -1,32 +1,36 @@
 import websocket
-from threading import Thread
-from tools.JSONTools import *
+from threading import Thread, Event
+import json
+import time
+
 
 class WSClient(Thread):
     def __init__(self, uri, delegate=None):
         super().__init__()
         self.uri = uri
         self.delegate = delegate
+        self.connected = False
+        self.uid = None
+        self.stop_event = Event()
         self.ws = websocket.WebSocketApp(uri,
                                          on_open=self.on_open,
                                          on_message=self.on_message,
                                          on_error=self.on_error,
                                          on_close=self.on_close)
-        self.connected = False
-        self.uid = None
 
     def run(self):
         self.ws.run_forever()
 
     def send_message(self, text):
-        message = {
-            "uid": self.uid,
-            "text": text
-        }
-        print(f"Sending message: {message}")
-        message_to_send = json_encode(message)
-        if message_to_send:
-            self.ws.send(message_to_send)
+        if self.connected:
+            message = {
+                "uid": self.uid,
+                "text": text
+            }
+            print(f"Sending message: {message}")
+            self.ws.send(json.dumps(message))
+        else:
+            print("Cannot send message, not connected")
 
     def on_open(self, ws):
         self.connected = True
@@ -34,25 +38,42 @@ class WSClient(Thread):
             self.delegate.on_open()
 
     def on_message(self, ws, message):
-        json_message = json_decode(message)
-        if json_message:
-            if "uid" in json_message:
-                self.uid = json_message["uid"]
-            if self.delegate:
-                self.delegate.on_message(json_message)
+        json_message = json.loads(message)
+        if "uid" in json_message:
+            self.uid = json_message["uid"]
+        if self.delegate:
+            self.delegate.on_message(json_message)
 
     def on_error(self, ws, error):
         if self.delegate:
             self.delegate.on_error(ws, error)
 
-    def on_close(self, ws):
+    def on_close(self, ws, close_status_code, close_msg):
         self.connected = False
         if self.delegate:
             self.delegate.on_close()
 
+        while not self.connected and not self.stop_event.is_set():
+            try:
+                self.ws = websocket.WebSocketApp(self.uri,
+                                                 on_open=self.on_open,
+                                                 on_message=self.on_message,
+                                                 on_error=self.on_error,
+                                                 on_close=self.on_close)
+                self.ws.run_forever()
+            except Exception as e:
+                print(f"Reconnection failed: {e}")
+            time.sleep(5)
+
+    def stop(self):
+        self.stop_event.set()
+        if self.ws:
+            self.ws.close()
+
     @staticmethod
     def connectToVPS(delegate=None):
-        uri = "ws://{ip}:{port}".format(ip="websocket.rezurrection.website", port=8765)
+        uri = "ws://{ip}:{port}".format(
+            ip="websocket.rezurrection.website", port=8765)
         return WSClient(uri, delegate)
 
     @staticmethod
