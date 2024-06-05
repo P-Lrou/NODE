f30092302f8c0be743f1919ad15a009f7d767372
Kibishi47 <moulin.emmanuel47@gmail.com>
2024-06-05 10:55:58 +0200

    feat: automatic on/off printer

--- IOT/tools/Printer.py
+++ IOT/tools/Printer.py
@@ -1,4 +1,6 @@
 import subprocess
+import RPi.GPIO as GPIO
+import time
 
 class Printer:
     @staticmethod
@@ -10,3 +12,13 @@ class Printer:
             '-i', image_path
         ]
         subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    @staticmethod
+    def switch_state(pin=18):
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(pin, GPIO.LOW)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+Printer.switch_state()
