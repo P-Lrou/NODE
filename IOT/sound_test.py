c272e07ee40578f3b77a6e88637db5e19b6adf43
Kibishi47 <moulin.emmanuel47@gmail.com>
2024-06-08 10:07:36 +0200

    test: sound

--- IOT/sound_test.py
+++ IOT/sound_test.py
@@ -1,22 +1,33 @@
 import subprocess
 from tools.DLog import DLog
+from GlobalVariables import Path
 
 class PlaySound:
     
     init_path = Path.instance().init_sound
     HDMI_JACK_CONTROL = 'numid=1'
 
-    @staticmethod
-    def set_volume(volume):
+    @classmethod
+    def __set_volume(cls, volume):
         # Assurez-vous que le volume est une valeur entre 0 et 100
         volume = max(0, min(100, volume))
-        # subprocess.run(['amixer', 'cset', cls.HDMI_JACK_CONTROL, f'{volume}%'])
+        # Mettre à jour le volume pour HDMI Jack avec sudo
+        try:
+            subprocess.run(['sudo', 'amixer', 'cset', cls.HDMI_JACK_CONTROL, f'{volume}%'], check=True)
+        except subprocess.CalledProcessError as e:
+            DLog.LogError(f"Failed to set volume: {e}")
+            print(f"Failed to set volume: {e}")
 
-    @staticmethod
-    def play_sound(file, volume=100):
+    @classmethod
+    def __play_sound(cls, file, volume=100):
         DLog.LogSuccess("PLAY SOUND")
         PlaySound.__set_volume(volume)
-        # subprocess.run(['aplay', '-D', 'hw:0,0', file])
+        # Jouer le son en utilisant mpg123 avec sudo
+        try:
+            subprocess.run(['sudo', 'mpg123', '-a', 'hw:0,0', file], check=True)
+        except subprocess.CalledProcessError as e:
+            DLog.LogError(f"Failed to play sound: {e}")
+            print(f"Failed to play sound: {e}")
 
     @classmethod
     def play_sound(cls, file_name, volume=100):
